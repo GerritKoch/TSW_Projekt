799577504618bb23b0c49474bd9a1901b3742ad8
gabriel.amo-takyi <gabriel.amo-takyi@student.fh-kiel.de>
2024-06-27 12:07:32 +0200

    check partly done

--- src/main/java/aof/Bag.java
+++ src/main/java/aof/Bag.java
@@ -1,38 +1,53 @@
 package aof;
 
 import de.fhkiel.tsw.armyoffrogs.Color;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+
+/**
+ * Represents the bag of frogs.
+ */
 public class Bag {
-    private List<Color> frogs;
+  private List<Color> frogs;
 
-    public Bag(){
+  public Bag() {
 
-        this(new Color[]{Color.Red, Color.Green, Color.Blue, Color.White});
+    this(new Color[] {Color.Red, Color.Green, Color.Blue, Color.White});
+  }
+
+  /**
+   * Creates a new bag with the given players.
+   *
+   * @param players The players to create the bag for.
+   */
+  public Bag(Color[] players) {
+    List<Color> newFrogList = new ArrayList<>();
+    for (Color player : players) {
+      for (int i = 0; i < 10; ++i) {
+        newFrogList.add(player);
+      }
+      frogs = newFrogList;
     }
+  }
 
-    public Bag(Color[] players) {
-        List<Color> newFrogList = new ArrayList<>();
-        for(Color player : players){
-            for(int i = 0; i < 10; ++i){
-                newFrogList.add(player);
-            }
-            frogs = newFrogList;
-        }
-    }
+  public int getFrogs() {
+    return frogs.size();
+  }
 
-    public int getFrogs() {
-        return frogs.size();
-    }
+  /**
+   * Takes a frog from the bag.
+   *
+   * @return The color of the frog.
+   */
+  public Color takeFrog() {
+    if (!frogs.isEmpty()) {
 
-    public Color takeFrog() {
-        if(!frogs.isEmpty()) {
-            return frogs.remove(new Random().nextInt(frogs.size()));
-        } else {
-            return Color.None;
-        }
+      var sample = new Random().nextInt(frogs.size());
+      return frogs.remove(sample);
+    } else {
+      return Color.None;
     }
+  }
 }
--- src/main/java/aof/Gamelogic.java
+++ src/main/java/aof/Gamelogic.java
@@ -3,95 +3,97 @@ package aof;
 import de.fhkiel.tsw.armyoffrogs.Color;
 import de.fhkiel.tsw.armyoffrogs.Game;
 import de.fhkiel.tsw.armyoffrogs.Position;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Represents the game logic.
+ */
 public class Gamelogic implements Game {
 
-    private Color[] player;
+  private Color[] player;
 
-    @Override
-    public boolean newGame(int numberOfPlayers) {
-        if(numberOfPlayers >= 2 && numberOfPlayers <= 4){
-            player = new Color[numberOfPlayers];
+  @Override
+  public boolean newGame(int numberOfPlayers) {
+    if (numberOfPlayers >= 2 && numberOfPlayers <= 4) {
+      player = new Color[numberOfPlayers];
 
-            Color[] colorOrder = {Color.Red, Color.Green, Color.Blue, Color.White};
-            for(int i = 0; i < numberOfPlayers; ++i){
-                player[i] = colorOrder[i];
-            }
+      Color[] colorOrder = {Color.Red, Color.Green, Color.Blue, Color.White};
+      for (int i = 0; i < numberOfPlayers; ++i) {
+        player[i] = colorOrder[i];
+      }
 
-            startGame(numberOfPlayers);
+      startGame(numberOfPlayers);
 
-            return true;
-        } else {
-            player = new Color[0];
-            return false;
-        }
+      return true;
+    } else {
+      player = new Color[0];
+      return false;
     }
+  }
 
-    @Override
-    public Color[] players() {
-        return player;
+  @Override
+  public Color[] players() {
+    return player;
+  }
+
+  @Override
+  public String getInfo() {
+    return null;
+  }
+
+  @Override
+  public List<Color> getFrogsInHand(Color player) {
+    return null;
+  }
+
+  @Override
+  public Set<Position> getBoard() {
+    return null;
+  }
+
+  @Override
+  public void clicked(Position position) {
+
+  }
+
+  @Override
+  public void selectedFrogInHand(Color player, Color frog) {
+
+  }
+
+  @Override
+  public Color winner() {
+    return null;
+  }
+
+  @Override
+  public boolean save(String filename) {
+    return false;
+  }
+
+  @Override
+  public boolean load(String filename) {
+    return false;
+  }
+
+  private Bag bag = new Bag();
+
+  @Override
+  public int frogsInBag() {
+    return bag.getFrogs();
+  }
+
+  private void startGame(int spieler) {
+    bag = new Bag(players());
+    for (int i = 0; i < 2 * spieler; ++i) {
+      bag.takeFrog();
     }
+  }
 
-    @Override
-    public String getInfo() {
-        return null;
-    }
-
-    @Override
-    public List<Color> getFrogsInHand(Color player) {
-        return null;
-    }
-
-    @Override
-    public Set<Position> getBoard() {
-        return null;
-    }
-
-    @Override
-    public void clicked(Position position) {
-
-    }
-
-    @Override
-    public void selectedFrogInHand(Color player, Color frog) {
-
-    }
-
-    @Override
-    public Color winner() {
-        return null;
-    }
-
-    @Override
-    public boolean save(String filename) {
-        return false;
-    }
-
-    @Override
-    public boolean load(String filename) {
-        return false;
-    }
-
-    private Bag bag = new Bag();
-
-    @Override
-    public int frogsInBag() {
-        return bag.getFrogs();
-    }
-
-    private void startGame(int spieler) {
-        bag = new Bag(players());
-        for (int i = 0; i < 2*spieler; ++i){
-            bag.takeFrog();
-        }
-    }
-
-    public void takeFrogFromBag() {
-        bag.takeFrog();
-    }
+  public void takeFrogFromBag() {
+    bag.takeFrog();
+  }
 
 }
