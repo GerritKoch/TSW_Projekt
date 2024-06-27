f6cdab6cb4a3a1ab93184d2b2b203b465a8edb24
gabriel.amo-takyi <gabriel.amo-takyi@student.fh-kiel.de>
2024-06-27 12:26:47 +0200

    google checks for frog done

--- src/main/java/de/fhkiel/tsw/Frog.java
+++ src/main/java/de/fhkiel/tsw/Frog.java
@@ -2,69 +2,71 @@ package de.fhkiel.tsw;
 
 import de.fhkiel.tsw.armyoffrogs.Color;
 import de.fhkiel.tsw.armyoffrogs.Position;
-
 import java.util.List;
 
+/**
+ * This class represents the Frogs in the game.
+ */
 public class Frog {
 
 
-    private Color frogColor;
-    private boolean frogInGame;
-    private boolean frogInHand = false;
-    private Position position;
+  private Color frogColor;
+  private boolean frogInGame;
+  private boolean frogInHand = false;
+  private Position position;
 
-    public List<Frog> getMyNeighbors() {
-        return myNeighbors;
-    }
+  public List<Frog> getMyNeighbors() {
+    return myNeighbors;
+  }
 
-    public void setMyNeighbors(List<Frog> myNeighbors) {
-        this.myNeighbors = myNeighbors;
-    }
+  public void setMyNeighbors(List<Frog> myNeighbors) {
+    this.myNeighbors = myNeighbors;
+  }
 
-    public void addNeighbor(Frog neighbor){
-        myNeighbors.add(neighbor);
-    }
+  public void addNeighbor(Frog neighbor) {
+    myNeighbors.add(neighbor);
+  }
 
-    private List<Frog> myNeighbors;
+  private List<Frog> myNeighbors;
 
 
-    public Frog (){
-        this.frogColor = Color.None;
-    }
+  public Frog() {
+    this.frogColor = Color.None;
+  }
 
-    public Frog(Color inputColor){
-        this.frogColor = inputColor;
-    }
+  public Frog(Color inputColor) {
+    this.frogColor = inputColor;
+  }
 
-    public Color getFrogColor() {
-        return frogColor;
-    }
+  public Color getFrogColor() {
+    return frogColor;
+  }
 
-    public boolean isFrogInGame() {
-        return frogInGame;
-    }
+  public boolean isFrogInGame() {
+    return frogInGame;
+  }
 
-    public void setFrogInGame(boolean frogInGame) {
-        this.frogInGame = frogInGame;
-    }
+  public void setFrogInGame(boolean frogInGame) {
+    this.frogInGame = frogInGame;
+  }
 
-    public void setFrogColor(Color frogColor) {
-        this.frogColor = frogColor;
-    }
+  public void setFrogColor(Color frogColor) {
+    this.frogColor = frogColor;
+  }
 
-    public boolean isFrogInHand() {
-        return frogInHand;
-    }
+  public boolean isFrogInHand() {
+    return frogInHand;
+  }
 
-    public void setFrogInHand(boolean frogInHand) {
-        this.frogInHand = frogInHand;
-    }
+  public void setFrogInHand(boolean frogInHand) {
+    this.frogInHand = frogInHand;
+  }
 
-    public Position getPosition() {
-        return position;
-    }
+  public Position getPosition() {
+    return position;
+  }
 
-    public void setPosition(Position position) {
-        this.position = position;
-    }
+  public void setPosition(Position position) {
+    this.position = position;
+  }
 }
